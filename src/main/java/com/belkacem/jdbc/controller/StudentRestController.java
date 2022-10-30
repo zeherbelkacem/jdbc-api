1f25fe65fc5ba83dbc3ccef85a93400992acfbfd
belkacem <59140588+zeherbelkacem@users.noreply.github.com>
2022-10-30 23:02:07 +0100

    Update StudentRestController.java

--- src/main/java/com/belkacem/jdbc/controller/StudentRestController.java
+++ src/main/java/com/belkacem/jdbc/controller/StudentRestController.java
@@ -1,7 +1,6 @@
 package com.belkacem.jdbc.controller;
 
 import java.util.List;
-import org.slf4j.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
